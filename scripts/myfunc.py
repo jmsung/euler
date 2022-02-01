7506d821637776d02ad9ad9aa68318981fea34af
Jongmin Sung <jongmin.sung@gmail.com>
2022-02-01 09:49:40 -0800

    update myfunc: sign, num2lst, lst2num update unittest

--- scripts/myfunc.py
+++ scripts/myfunc.py
@@ -153,3 +153,42 @@ def divisors(n):
             divisors.add(int(n / i))
 
     return list(divisors)
+
+
+def sign(n):
+    """ Return sign 1 (n >= 0) or -1 (n < 0) """
+
+    if not (isinstance(n, float) or isinstance(n, int)):
+        raise ValueError("Input must be real number.")
+
+    import math
+    return math.copysign(1, n)
+
+
+def num2lst(n):
+    """ Convert from a multiple digit number to a list of single digit """
+    return [int(i) if n > 0 else -int(i) for i in str(abs(n))]
+
+
+def lst2num(lst):
+    """ Convert from a list of single digits to a multiple digit number """
+
+    # check all the element have the same sign
+    if not all(sign(element) == sign(lst[0]) for element in lst if element != 0):
+        raise ValueError("All the element in the list must have the same sign.")
+
+    num = int("".join([str(abs(i)) for i in lst]))
+    return num if sign(lst[0]) > 0 else -num
+
+
+def list_same_digit(n):
+    """ Return the sorted list of the same digits from the input number
+
+    Parameters
+    ----------
+    n (int): An int number
+
+    Return
+    ------
+    digits (list of int): combination of digits """
+    pass
--- scripts/tests/test_myfunc.py
+++ scripts/tests/test_myfunc.py
@@ -3,7 +3,7 @@ import unittest
 
 from scripts.myfunc import (
     fib, prime_factor, is_prime, primes_below, element_product,
-    greatest_product, divisors,
+    greatest_product, divisors, sign, lst2num, num2lst,
 )
 
 
@@ -71,3 +71,28 @@ class TestMyFunc(unittest.TestCase):
         ]
         for test_input, test_output in test_data:
             self.assertEqual(divisors(test_input), test_output)
+
+    def test_sign(self):
+        test_data = [
+            (2, 1),
+            (0, 1),
+            (-2.6, -1),
+        ]
+        for test_input, test_output in test_data:
+            self.assertEqual(sign(test_input), test_output)
+
+    def test_num2lst(self):
+        test_data = [
+            (236, [2, 3, 6]),
+            (-702, [-7, 0, -2]),
+        ]
+        for test_input, test_output in test_data:
+            self.assertEqual(num2lst(test_input), test_output)
+
+    def test_lst2num(self):
+        test_data = [
+            ([2, 3, 6], 236),
+            ([-7, 0, -2], -702),
+        ]
+        for test_input, test_output in test_data:
+            self.assertEqual(lst2num(test_input), test_output)
